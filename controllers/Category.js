64b9c93f1c67a6edd95bb5b034483d1f03b2052a
Rishabh523 <parishabh8787@gmail.com>
2023-11-03 22:15:45 +0530

    fourth commit

--- controllers/Category.js
+++ controllers/Category.js
@@ -54,3 +54,44 @@ exports.showAllCategories = async (req, res) => {
          });
     }
 }
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        //get category id
+        const {categoryId} = req.body;
+        //get courses for specified category id
+        const selectedCategory = await Category.findById(categoryId)
+                                        .populate("courses")
+                                        .exec();
+        //validation
+        if(!selectedCategory){
+            return res.status(404).json({
+              success:false,
+              message:"Data Not Found",  
+            });
+        }
+        // get courses for different categories
+        const differentCategories = await Category.find({
+                                        _id:{$ne: categoryId},
+        })
+        .populate("courses")
+        .exec();
+        //get top 10 selling courses
+        //hw->write it on own
+        //return response
+        return res.status(200).json({
+            success:true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            },
+        });
+
+    } catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+}
